8ea70a84e0a4d50e10bc67de2107420b78523887
sharathujiredatamatics <sharath.ujire@digital.datamatics.com>
2022-03-08 14:25:21 +0530

    SDK

---
+++ MobScannerSDK.framework/Headers/MobScannerSDK.h
@@ -0,0 +1,19 @@
+//
+//  MobScannerSDK.h
+//  MobScannerSDK
+//
+//  Created by TechVariable on 01/01/21.
+//  Copyright © 2021 TechVariable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "OpenCVWrapper.h"
+//! Project version number for MobScannerSDK.
+FOUNDATION_EXPORT double MobScannerSDKVersionNumber;
+
+//! Project version string for MobScannerSDK.
+FOUNDATION_EXPORT const unsigned char MobScannerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobScannerSDK/PublicHeader.h>
+
+
